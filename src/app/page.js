8ec85653b1c91507eaea92d06522e637dfd1745d
ZeetGeek <150018728+ZeetGeek@users.noreply.github.com>
2024-01-29 16:57:12 +0530

    create header

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,10 @@
-import Image from "next/image";
+import { Header } from "./components/header";
 
 export default function Home() {
 	return (
 		<>
+			<Header />
+
 			<button className='py-3 px-5 bg-black text-white rounded-md shadow-lg mt-5 ms-5'>Button</button>
 			<button className='py-3 px-5 bg-blue-500 text-white rounded-md shadow-lg shadow-blue-500/20 duration-150 hover:bg-blue-600 mx-5'>Designer Button</button>
 			<button className='py-3 px-5 bg-indigo-500 text-white rounded-md shadow-lg shadow-indigo-500/20 duration-150 hover:bg-indigo-600'>Developer Button</button>
