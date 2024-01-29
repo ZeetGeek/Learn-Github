cbaf7602b8c168c6239a5eaf04c6a7300ebf3216
ZeetGeek <150018728+ZeetGeek@users.noreply.github.com>
2024-01-29 16:13:36 +0530

    create designer button

--- src/app/page.js
+++ src/app/page.js
@@ -4,6 +4,7 @@ export default function Home() {
 	return (
 		<>
 			<button className='py-3 px-5 bg-black text-white rounded-md shadow-lg m-5'>Button</button>
+			<button className='py-3 px-5 bg-blue-500 text-white rounded-md shadow-lg shadow-blue-500/20 duration-150 hover:bg-blue-600'>Designer Button</button>
 		</>
 	);
 }
